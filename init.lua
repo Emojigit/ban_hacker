6ae144e176627092d425a01440105543e9eb26a8
Yiu Man Ho <yiufamily.hh@gmail.com>
2020-08-17 18:12:35 +0800

    Will send mail to admin when a hacker detect

--- init.lua
+++ init.lua
@@ -1,4 +1,7 @@
 local mod_storage = minetest.get_mod_storage()
+local have_mod = {}
+have_mod.mail = minetest.get_modpath("mail")
+have_mod.default = minetest.get_modpath("default")
 local worlddir = minetest.get_worldpath()
 local rc = {}
 rc.warn = {}
@@ -8,6 +11,17 @@ local warn_time = 5-2
 local warn_second = 300
 local ban_second = 300
 local banip_second = 600
+if have_mod.mail and core.settings:get("name") then
+	rc.send_mail = function(name, ip, time, type_login)
+		local title = "Ban Hacker log: "..name.." ["..ip.."] at "..tostring(time)
+		local text_m1 = "Type: "
+		local text_0 = text_m1..(type_login or "Ban")
+		local text_1 = "A user tried to hack the server at "..tostring(time)..".\n"..name.." ["..ip.."]\n"
+		local text_3 = "\nDO NOT REPLY THIS MESSAGE\n\nFrom,\nSystem"
+		local text = text_1..(text_2 or "")..text_3
+		mail.send("Ban Hackers System",core.settings:get("name"),title,text)
+	end
+end
 
 rc.record = function(target)
 	local type = "Warn"
@@ -44,6 +58,9 @@ minetest.register_on_authplayer(function(name, ip, is_success)
 	if not(is_success) then
 		rc.record(name)
 		rc.record(ip)
+		if (rc.ban[name] or rc.ban[ip]) and rc.send_mail then
+			rc.send_mail(name, ip, os.time(os.date("!*t")))
+		end
 		minetest.log("action", "[ban_hacker] Wrong password from "..name.." ["..ip.."], recorded.")
 	end
 end)
@@ -55,6 +72,7 @@ minetest.register_on_prejoinplayer(function(name, ip)
 		minetest.log("action", "[ban_hacker] Rejected connect from "..name.." ["..ip.."]")
 		if (rc.ban[ip] + banip_second > os.time(os.date("!*t"))) then
 			rc.record(ip)
+			rc.send_mail(name, ip, os.time(os.date("!*t")), "Tried to join")
 		end
 		return "Too many wrong password for this account or IP.".."\nTry again later."
 	end
