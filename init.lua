b287ad26215e3cf85ee1253a69bbb732f62b0708
Yiu Man Ho <yiufamily.hh@gmail.com>
2021-08-01 08:25:37 +0800

    bugfix

--- init.lua
+++ init.lua
@@ -3,16 +3,16 @@ local have_mod = {}
 have_mod.mail = minetest.get_modpath("mail")
 have_mod.default = minetest.get_modpath("default")
 local worlddir = minetest.get_worldpath()
-local rc = {}
-rc.warn = {}
-rc.warn_count = {}
-rc.ban = {}
+local bh_rc = {}
+bh_rc.warn = {}
+bh_rc.warn_count = {}
+bh_rc.ban = {}
 local warn_time = 5-2
 local warn_second = 300
 local ban_second = 300
 local banip_second = 600
 if have_mod.mail and core.settings:get("ban_hacker_mail") then
-	rc.send_mail = function(name, ip, time, type_login)
+	bh_rc.send_mail = function(name, ip, time, type_login)
 		local title = "Ban Hacker log: "..name.." ["..ip.."] at "..tostring(time)
 		local text_m1 = "Type: "
 		local text_0 = text_m1..(type_login or "Ban")
@@ -23,56 +23,56 @@ if have_mod.mail and core.settings:get("ban_hacker_mail") then
 	end
 end
 
-rc.record = function(target)
+bh_rc.record = function(target)
 	local type = "Warn"
-	if not(rc.warn_count[target]) then
-		rc.warn_count[target] = 0
+	if not(bh_rc.warn_count[target]) then
+		bh_rc.warn_count[target] = 0
 	end
-	if rc.warn[target] and ((rc.warn[target] + warn_second) > os.time(os.date("!*t"))) then
-		rc.warn_count[target] = rc.warn_count[target] + 1
+	if bh_rc.warn[target] and ((bh_rc.warn[target] + warn_second) > os.time(os.date("!*t"))) then
+		bh_rc.warn_count[target] = bh_rc.warn_count[target] + 1
 	end
-	rc.warn[target] = os.time(os.date("!*t"))
-	if rc.warn_count[target] > warn_time then
-		rc.ban[target] = os.time(os.date("!*t"))
+	bh_rc.warn[target] = os.time(os.date("!*t"))
+	if bh_rc.warn_count[target] > warn_time then
+		bh_rc.ban[target] = os.time(os.date("!*t"))
 		local type = "Ban"
 	end
 	minetest.log("action", "[ban_hacker] Recored `"..target.."`, type "..type..".")
-	rc.save(rc.ban)
+	bh_rc.save(bh_rc.ban)
 end
 
-rc.save = function(data)
+bh_rc.save = function(data)
 	mod_storage:set_string("ban_rc", minetest.serialize(data))
 end
 
-rc.get = function()
+bh_rc.get = function()
 	return minetest.deserialize(mod_storage:get_string("ban_rc"))
 end
 
 -------------------------------------
 
-rc.ban = rc.get() or {}
+bh_rc.ban = bh_rc.get() or {}
 
 -------------------------------------
 
 minetest.register_on_authplayer(function(name, ip, is_success)
 	if not(is_success) then
-		rc.record(name)
-		rc.record(ip)
-		if (rc.ban[name] or rc.ban[ip]) and rc.send_mail then
-			rc.send_mail(name, ip, os.date('%Y-%m-%d %H:%M:%S'))
+		bh_rc.record(name)
+		bh_rc.record(ip)
+		if (bh_rc.ban[name] or bh_rc.ban[ip]) and bh_rc.send_mail then
+			bh_rc.send_mail(name, ip, os.date('%Y-%m-%d %H:%M:%S'))
 		end
 		minetest.log("action", "[ban_hacker] Wrong password from "..name.." ["..ip.."], recorded.")
 	end
 end)
 
 minetest.register_on_prejoinplayer(function(name, ip)
-	rc.ban[name] = rc.ban[name] or 0
-	rc.ban[ip] = rc.ban[ip] or 0
-	if ((rc.ban[name] + ban_second > os.time(os.date("!*t"))) or (rc.ban[ip] + banip_second > os.time(os.date("!*t")))) and not(ip == "127.0.0.1") then
+	bh_rc.ban[name] = bh_rc.ban[name] or 0
+	bh_rc.ban[ip] = bh_rc.ban[ip] or 0
+	if ((bh_rc.ban[name] + ban_second > os.time(os.date("!*t"))) or (bh_rc.ban[ip] + banip_second > os.time(os.date("!*t")))) and not(ip == "127.0.0.1") then
 		minetest.log("action", "[ban_hacker] Rejected connect from "..name.." ["..ip.."]")
-		if (rc.ban[ip] + banip_second > os.time(os.date("!*t"))) then
-			rc.record(ip)
-			rc.send_mail(name, ip, os.date('%Y-%m-%d %H:%M:%S'), "Tried to join")
+		if (bh_rc.ban[ip] + banip_second > os.time(os.date("!*t"))) then
+			bh_rc.record(ip)
+			bh_rc.send_mail(name, ip, os.date('%Y-%m-%d %H:%M:%S'), "Tried to join")
 		end
 		return "Too many wrong password for this account or IP.".."\nTry again later."
 	end
